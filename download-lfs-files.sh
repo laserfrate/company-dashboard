3377184c82198dcd071712c37934ba75f5b3762a
mihaiconstantinescu <mihaiconstantinescu0505@gmail.com>
2024-06-18 02:52:47 +0300

    Added script to download LFS files

--- download-lfs-files.sh
+++ download-lfs-files.sh
@@ -6,6 +6,10 @@ set -e
 # Print each command before executing it
 set -x
 
+# Install git-lfs
+curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | sudo bash
+sudo apt-get install git-lfs
+
 # Fetch all LFS objects
 git lfs install
 git lfs fetch
